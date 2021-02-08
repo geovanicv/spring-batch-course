abe1ab8f1055f3d505390c693cbf1764deb85ceb
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2021-02-07 22:35:16 -0300

    First spring batch project

---
+++ 1-Introduction/firstSpringBatchProject/src/test/java/com/geovanicv/firstSpringBatchProject/FirstSpringBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovanicv.firstSpringBatchProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
